0a20af2424443a44733a157406074e106a5bf0d2
Christopher Day <dayo@cddesigns.org>
2014-01-06 20:48:27 +0000

    Added Default timezone
    Due to me not setting up php properly on my test server i have to set
    the default timezone.

--- config.php
+++ config.php
@@ -1,6 +1,9 @@
 <?php
 
-	$page->addToTemplate('game_name', 'Game Name'); // The name of your game
+    // Set the default timezone to london
+    date_default_timezone_set('Europe/London'); 
+
+	$page->addToTemplate('game_name', 'Gangster Legends V2'); // The name of your game
 	$page->theme = 'default'; // select the template you want to load
 	
 ?>
