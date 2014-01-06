450c3d37f997753bb430ee17007d53fb8eb170df
Christopher Day <dayo@cddesigns.org>
2014-01-06 21:04:54 +0000

    Removed old files
    The login/register/logout were not part of the modular system until i
    converted them today, the old files are nor redundant so can be removed.

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
     // Set the default timezone to london
     date_default_timezone_set('Europe/London'); 
 
-	$page->addToTemplate('game_name', 'Gangster Legends V2'); // The name of your game
+	$page->addToTemplate('game_name', 'Gangster Legends'); // The name of your game
 	$page->theme = 'default'; // select the template you want to load
 	
 ?>
