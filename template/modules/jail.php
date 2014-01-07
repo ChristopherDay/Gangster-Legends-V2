cd90b8a8f03f9bc42fe3075551d5045e74d9ba65
Christopher Day <dayo@cddesigns.org>
2014-01-07 14:06:53 +0000

    Added jail template file
    I forgot to make the jail template file, in this commit i have added it

---
+++ template/modules/jail.php
@@ -0,0 +1,10 @@
+<?php
+
+    class jailTemplate extends template {
+    
+        public $loginPage = false; // Ture means you can access this page without being logged in
+        public $jailPage = true; // True means you can view this page in prison
+        
+    }
+
+?>
