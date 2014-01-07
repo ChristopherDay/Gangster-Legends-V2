85c0e5074e2d5d63feff2d49ce261a020eaf6ceb
Christopher Day <dayo@cddesigns.org>
2014-01-07 12:25:56 +0000

    Bug fixes + Additions
    1) Renamed start.php to init.php
    2) Fixed a bug where the template was not showing correct user values
    until you refreshed
    3) Added several function to the module class where you can request
    information easily without having to write several lines of code.

--- class/module.php
+++ class/module.php
@@ -17,6 +17,16 @@
             
             $this->constructModule();
             
+            if (isset($user->info->U_id)) {
+            
+                // Update the user info after the module has run.
+                $this->user->getInfo();
+                
+                // Bind the varibles to the template
+                $this->user->bindVarsToTemplate();
+                
+            }
+            
         }
     
         public function timeLeft($ts) {
@@ -29,31 +39,96 @@
         
         public function getLocation($locationID, $info = false) {
         
-            // Coming soon
+            $loc = $this->db->prepare("SELECT * FROM locations WHERE L_id = :location");
+            $loc->bindParam(":location", $locationID);
+            $loc->execute();
             
+            $location = $loc->fetchObject();
+            
+            if (isset($location->L_id)) {
+                
+                if ($info) {
+                    return $location;
+                } else {
+                    return $location->L_name;
+                }
+                
+            } else {
+                
+                return 'Location dosent exist!';
+                
+            }
         }
         
         public function getCar($carID, $info = false) {
         
-            // Coming soon
+            $car = $this->db->prepare("SELECT * FROM cars WHERE CA_id = :car");
+            $car->bindParam(":car", $carID);
+            $car->execute();
+            
+            $carInfo = $car->fetchObject();
+            
+            if (isset($carInfo->CA_id)) {
+                
+                if ($info) {
+                    return $carInfo;
+                } else {
+                    return $carInfo->CA_name;
+                }
+                
+            } else {
+                
+                return 'Car dosent exist!';
+                
+            }
             
         }
         
         public function getWeapon($weaponID, $info = false) {
         
-            // Coming soon
+            $wep = $this->db->prepare("SELECT * FROM weapons WHERE W_id = :weapon");
+            $wep->bindParam(":weapon", $weaponID);
+            $wep->execute();
+            
+            $weapon = $wep->fetchObject();
+            
+            if (isset($weapon->W_id)) {
+                
+                if ($info) {
+                    return $weapon;
+                } else {
+                    return $weapon->W_name;
+                }
+                
+            } else {
+                
+                return 'Weapon dosent exist!';
+                
+            }
         
         }
         
         public function getRank($rankID, $info = false) {
         
-            // Coming soon
-        
-        }
-        
-        public function checkTimer($timer, $returnTime = false) {
-        
-            // Coming soon
+            $rank = $this->db->prepare("SELECT * FROM ranks WHERE R_id = :rank");
+            $rank->bindParam(":rank", $rankID);
+            $rank->execute();
+            
+            $rankInfo = $rank->fetchObject();
+            
+            if (isset($rankInfo->R_id)) {
+                
+                if ($info) {
+                    return $rankInfo;
+                } else {
+                    return $rankInfo->R_name;
+                }
+                
+            } else {
+                
+                return 'Rank dosent exist!';
+                
+            }
         
         }
         
