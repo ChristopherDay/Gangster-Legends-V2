34207fdd09e9acfdf5ed965289c8e21c4274d023
Christopher Day <dayo@cddesigns.org>
2014-04-03 14:06:51 +0100

    Updated SQL
    This is the updated database structure for Gangster Legends V2.0.1

--- sql.sql
+++ sql.sql
@@ -1,25 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.2.4
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jan 06, 2014 at 07:42 PM
--- Server version: 5.1.44
--- PHP Version: 5.3.1
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `MobGame`
---
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `cars`
@@ -84,19 +62,14 @@ CREATE TABLE IF NOT EXISTS `gameNews` (
   `GN_text` text NOT NULL,
   `GN_date` int(11) NOT NULL,
   PRIMARY KEY (`GN_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `gameNews`
 --
 
 INSERT INTO `gameNews` (`GN_id`, `GN_author`, `GN_title`, `GN_text`, `GN_date`) VALUES
-(1, 1, 'Some Title 1', 'edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v ', 1388562841),
-(2, 1, 'Some Title 2', 'edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v ', 1388562801),
-(3, 1, 'Some Title 3', 'edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v ', 1388562741),
-(4, 1, 'Some Title 4', 'edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v ', 1388562701),
-(5, 1, 'Some Title 5', 'edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v ', 1388562641),
-(6, 1, 'Some Title 6', 'edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v edrjkkjlhvghbkj bkxj bvkjzxcb vkjzxb v ', 1388562601);
+(7, 1, 'Gangster Legends Installed', 'Gangster legends was successfully installed!', 1396528455);
 
 -- --------------------------------------------------------
 
@@ -131,15 +104,7 @@ CREATE TABLE IF NOT EXISTS `garage` (
   `GA_damage` int(11) NOT NULL,
   `GA_location` int(11) NOT NULL,
   PRIMARY KEY (`GA_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
-
---
--- Dumping data for table `garage`
---
-
-INSERT INTO `garage` (`GA_id`, `GA_uid`, `GA_car`, `GA_damage`, `GA_location`) VALUES
-(23, 1, 4, 0, 3),
-(22, 3, 4, 2, 1);
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=51 ;
 
 -- --------------------------------------------------------
 
@@ -162,7 +127,7 @@ CREATE TABLE IF NOT EXISTS `locations` (
 --
 
 INSERT INTO `locations` (`L_id`, `L_name`, `L_cost`, `L_bullets`, `L_bulletCost`, `L_cooldown`) VALUES
-(1, 'London', 170, 11977, 100, 3600),
+(1, 'London', 170, 11827, 100, 3600),
 (3, 'Rome', 220, 0, 100, 4800),
 (2, 'Paris', 200, 0, 100, 4200);
 
@@ -183,13 +148,6 @@ CREATE TABLE IF NOT EXISTS `mail` (
   PRIMARY KEY (`M_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
---
--- Dumping data for table `mail`
---
-
-INSERT INTO `mail` (`M_id`, `M_uid`, `M_sid`, `M_subject`, `M_text`, `M_type`, `M_read`) VALUES
-(1, 1, 2, 'cvdfdsfds', 'dsfdfadasfdasfad', 1, 0);
-
 -- --------------------------------------------------------
 
 --
@@ -203,13 +161,6 @@ CREATE TABLE IF NOT EXISTS `notifications` (
   `N_read` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`N_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `notifications`
---
-
-INSERT INTO `notifications` (`N_id`, `N_uid`, `N_text`, `N_read`) VALUES
-(1, 1, 'dsfdsasdsadsds', 0);
 
 -- --------------------------------------------------------
 
@@ -235,6 +186,27 @@ INSERT INTO `ranks` (`R_id`, `R_name`, `R_exp`, `R_limit`, `R_cashReward`, `R_bu
 (1, 'Lowlife', 20, 0, 75, 25),
 (2, 'Thug', 50, 0, 150, 60),
 (3, 'Criminal', 100, 0, 250, 100);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE IF NOT EXISTS `settings` (
+  `S_id` int(11) NOT NULL AUTO_INCREMENT,
+  `S_desc` varchar(120) NOT NULL,
+  `S_value` text NOT NULL,
+  PRIMARY KEY (`S_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`S_id`, `S_desc`, `S_value`) VALUES
+(4, 'game_name', 'Game Name'),
+(5, 'theme', 'default');
 
 -- --------------------------------------------------------
 
@@ -277,7 +249,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `U_userLevel` int(1) NOT NULL,
   `U_status` int(1) NOT NULL,
   PRIMARY KEY (`U_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `users`
@@ -305,20 +277,31 @@ CREATE TABLE IF NOT EXISTS `userStats` (
   `US_gang` int(11) NOT NULL DEFAULT '0',
   `US_location` int(11) NOT NULL DEFAULT '1',
   `US_crimes` varchar(255) NOT NULL DEFAULT '35-25-15-5-5-5-5-5-5-5-5-5-5-5-5',
-  `US_crimeTimer` int(11) NOT NULL DEFAULT '0',
-  `US_jailTimer` int(11) NOT NULL DEFAULT '0',
-  `US_theftTimer` int(11) NOT NULL DEFAULT '0',
-  `US_chaseTimer` int(11) NOT NULL DEFAULT '0',
-  `US_bulletTimer` int(11) NOT NULL DEFAULT '0',
-  `US_travelTimer` int(11) NOT NULL DEFAULT '0'
+  `US_bio` text,
+  `US_pic` varchar(128) NOT NULL DEFAULT '',
+  `US_bank` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `userStats`
 --
 
-INSERT INTO `userStats` (`US_id`, `US_health`, `US_exp`, `US_money`, `US_bullets`, `US_backfire`, `US_credits`, `US_weapon`, `US_rank`, `US_gang`, `US_location`, `US_crimes`, `US_crimeTimer`, `US_jailTimer`, `US_theftTimer`, `US_chaseTimer`, `US_bulletTimer`, `US_travelTimer`) VALUES
-(6, 100, 0, 250, 100, 50, 0, 1, 1, 0, 1, '35-25-15-5-5-5-5-5-5-5-5-5-5-5-5', 0, 0, 0, 0, 0, 0);
+INSERT INTO `userStats` (`US_id`, `US_health`, `US_exp`, `US_money`, `US_bullets`, `US_backfire`, `US_credits`, `US_weapon`, `US_rank`, `US_gang`, `US_location`, `US_crimes`, `US_bio`, `US_pic`, `US_bank`) VALUES
+(6, 100, 15, 167551, 945, 50, 0, 1, 3, 0, 1, '100-32-15-5-5-5-5-5-5-5-5-5-5-5-5', '', '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userTimer`
+--
+
+CREATE TABLE IF NOT EXISTS `userTimer` (
+  `UT_id` int(11) NOT NULL AUTO_INCREMENT,
+  `UT_user` int(11) NOT NULL,
+  `UT_desc` varchar(120) NOT NULL,
+  `UT_time` int(11) NOT NULL,
+  PRIMARY KEY (`UT_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
 
 -- --------------------------------------------------------
 
@@ -339,3 +322,7 @@ CREATE TABLE IF NOT EXISTS `weapons` (
 
 INSERT INTO `weapons` (`W_id`, `W_name`, `W_accuracy`) VALUES
 (1, 'Pistol', 5);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
