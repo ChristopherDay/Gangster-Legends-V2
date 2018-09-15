7b558b1b690ff5b2c4dac23c1d6a08e29f6ecdb1
Chris Day <chris@cdcoding.com>
2018-09-15 18:54:30 +0100

    added basic admin dashboard

--- modules/installed/admin/admin.tpl.php
+++ modules/installed/admin/admin.tpl.php
@@ -1,19 +1,45 @@
 <?php
 
     class adminTemplate extends template {
-        public $adminTemplate = '
-        	<h2>Module</h2>
-        	{#each modules}
-        		<a href="?page=admin&module={moduleName}">{name}</a> <br />
-        	{/each}
+        public $dashboard = '
+        	<div class="row">
+                <div class="col-md-6">
+                    <h3>Statistics</h3>
+                    <table class="table table-condensed table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Statistic</th>
+                                <th width="50px">#</th>
+                                <th width="100px">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Users</td>
+                                <td>{users}</td>
+                                <td>
+                                    <a href="?page=admin&module=users&action=view">
+                                        View
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Active Modules</td>
+                                <td>{modules}</td>
+                                <td>
+                                    <a href="?page=admin&module=moduleManager&action=view">
+                                        View
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-md-6">
+                </div>
+            </div>
         ';
 
-        public $moduleTemplate = '
-        	<h2>{module}</h2>
-        	{#each links}
-        		<a href="?page=admin&module={moduleName}">{name}</a> <br />
-        	{/each}
-        ';
     }
 
 ?>
