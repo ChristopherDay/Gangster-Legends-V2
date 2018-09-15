080deb3ebc63b83b3ba06f195f37d71dbd1d445f
Chris Day <chris@cdcoding.com>
2018-09-15 18:54:30 +0100

    added basic admin dashboard

---
+++ modules/installed/admin/admin.admin.php
@@ -0,0 +1,20 @@
+<?php
+	class adminModule {
+
+		public function method_view() {
+
+			$users = $this->db->prepare("
+				SELECT COUNT(*) as 'users' FROM users
+			");
+
+			$users->execute();
+
+
+			$this->html = $this->page->buildElement("dashboard", array(
+				"users" => $users->fetch(PDO::FETCH_ASSOC)["users"], 
+				"modules" => count($this->page->modules)
+			));
+		}
+
+	}
+?>
