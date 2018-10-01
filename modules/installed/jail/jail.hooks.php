5d38be8a6e5f8d999e5556e75d94e62a51ed9443
Chris Day <chris@cdcoding.com>
2018-10-01 19:39:42 +0100

    removed moduleInfo from hooks

--- modules/installed/jail/jail.hooks.php
+++ modules/installed/jail/jail.hooks.php
@@ -1,16 +1,4 @@
 <?php
-	$info = array(
-		"name" => "Jail", 
-		"version" => "1.0.0", 
-		"description" => "This module allows a user to see jail inmates",
-		"author" => array(
-			"name" => "Chris Day", 
-			"url" => "http://glscript.cdcoding.com"
-		), 
-		"pageName" => "Jail",
-		"accessInJail" => true, 
-		"requireLogin" => true
-	);
 
 	new hook("locationMenu", function () {
 		return array(
