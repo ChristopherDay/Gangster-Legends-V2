f13a8f7e9a1366e127c0cf4ba1434c8f89f3193b
Chris Day <chris@cdcoding.com>
2018-10-01 19:39:42 +0100

    removed moduleInfo from hooks

--- modules/installed/garage/garage.hooks.php
+++ modules/installed/garage/garage.hooks.php
@@ -1,17 +1,4 @@
 <?php
-	$info = array(
-		"name" => "Garage", 
-		"version" => "1.0.0", 
-		"description" => "This module allows a user to buy bullets",
-		"author" => array(
-			"name" => "Chris Day", 
-			"url" => "http://glscript.cdcoding.com"
-		), 
-		"pageName" => "Garage",
-		"accessInJail" => false, 
-		"requireLogin" => true
-	);
-
 	new hook("locationMenu", function () {
 		return array(
 			"url" => "?page=garage", 
