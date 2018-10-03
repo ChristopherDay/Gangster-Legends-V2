0265d1168f97beda456490d68ea74d80fc17f47a
Chris Day <chris@cdcoding.com>
2018-10-03 13:49:05 +0100

    Added support for module specific styles and a new mobile navigation

--- themes/default - Grey/loggedin.php
+++ themes/default - Grey/loggedin.php
@@ -9,32 +9,73 @@
 		<head>
 			<link href="themes/default - Grey/css/bootstrap.min.css" rel="stylesheet" />
 			<link href="themes/default - Grey/css/style.css" rel="stylesheet" />
+			{#if moduleCssFile}
+				<link href="{moduleCssFile}" rel="stylesheet" />
+			{/if}
 			<link rel="shortcut icon" href="themes/default - Grey/images/icon.png" />
+			<meta name="timestamp" content="{timestamp}">
 			<meta name="viewport" content="width=device-width, initial-scale=1">
 			<title>{game_name} - {page}</title>
 		</head>
 		<body>
+
+			<div class="mobile-menu">
+				<div class="close-mobile-menu">Close</div>
+				<ul class="nav nav-pills nav-stacked">
+					{#each menus}
+						<li class="title col-xs-12"><a href="#">{title}</a></li>
+						{#each items}
+							{#if seperator}
+								<hr />
+							{/if}
+							{#unless seperator}
+								{#unless hide}
+									<li class="col-xs-6"><a href="{url}">{text}</a></li>
+								{/unless}
+							{/unless}
+						{/each}
+					{/each}
+				</ul>
+			</div>
+
 			<div class="header">
 				<div class="container">
 					<div class="row">
 						<div class="col-md-4 logo-container text-left">
+							<button type="button" class="navbar-toggle">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
 							<img src="themes/default - Grey/images/logo.png" alt="Gangster Legends" />
 						</div>
-						<div class="col-md-2 col-xs-6 text-center">
-							<a href="?page=mail">
-								Mail {#if mail}({mail}){/if}<br />
-							</a>
-							<a href="?page=notifications">
-								Notifications {#if notifications} ({notifications}){/if}
-							</a>
+						<div class="col-md-2 col-xs-12 text-center">
+							<div class="row">
+								<div class="col-md-12 col-xs-6 text-center">
+									<a href="?page=mail">
+										Mail{#if mail} ({mail}){/if}<br />
+									</a>
+								</div>
+								<div class="col-md-12 col-xs-6 text-center">
+									<a href="?page=notifications">
+										Notifications {#if notifications} ({notifications}){/if}
+									</a>
+								</div>
+							</div>
 						</div>
 						<div class="col-md-3 col-xs-6 text-center">
-								<strong>Money:</strong> {money} <br />
-								<strong>Bullets:</strong> {bullets}
+								<strong class="hidden-xs">Money:</strong> {money} <br />
+								<div class="hidden-xs">
+									<strong>Bullets:</strong> {bullets}
+								</div>
 						</div>
 						<div class="col-md-3 col-xs-6 text-center">
-							<strong>Rank:</strong> {rank} {#unless maxRank}{exp_perc}{/unless}<br />
-							<strong>Family:</strong> {gang.name}
+							<strong class="hidden-xs">Rank:</strong> 
+							{rank} {#unless maxRank}{exp_perc}{/unless}<br />
+							<div class="hidden-xs">
+								<strong>Family:</strong> {gang.name}
+							</div>
 						</div>
 					</div>
 				</div>
@@ -84,11 +125,11 @@
 						<div class="col-md-3 navigation text-center"> 
 
 							{#each menus}
-								<div class="panel panel-default">
+								<div class="panel panel-default hidden-xs">
 									<div class="panel-heading">
 										{title}
 									</div>
-									<div class="panel-body hidden-xs">
+									<div class="panel-body">
 										{#each items}
 											{#if seperator}
 												<hr />
@@ -105,7 +146,7 @@
 
 						</div>
 						
-						<div class="col-md-9">
+						<div class="col-md-9 game-container">
 							<div class="panel panel-default">
 								<div class="panel-heading">
 									{page}
