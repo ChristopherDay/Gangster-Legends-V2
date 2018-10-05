ae283e87449a7afe8724d62ce1ee12844f16bde2
Chris Day <chris@cdcoding.com>
2018-10-05 18:43:42 +0100

    added notifications

---
+++ modules/installed/notifications/notifications.inc.php
@@ -0,0 +1,42 @@
+<?php
+
+    class notifications extends module {
+        
+        public $allowedMethods = array(
+        	"id" => array( "type" => "GET" ),
+        	"reply" => array( "type" => "GET" ),
+        	"message" => array( "type" => "POST" ),
+        	"subject" => array( "type" => "POST" )
+        );
+		
+		public $pageName = '';
+
+        public function constructModule() {
+
+            $notifications = $this->db->prepare("
+                SELECT
+                    N_read as 'read', 
+                    N_text as 'text', 
+                    N_time as 'time'
+                FROM notifications WHERE N_uid = :id
+                ORDER BY N_time DESC;
+                UPDATE notifications SET N_read = 1 WHERE N_uid = :id AND N_read = 0;
+            ");
+
+            $notifications->bindParam(":id", $this->user->id);
+            $notifications->execute();
+            $notifications = $notifications->fetchAll(PDO::FETCH_ASSOC);
+
+            foreach ($notifications as $key => $value) {
+                $notifications[$key]["date"] = date("jS M H:i", $value["time"]);
+            }
+
+            $this->html = $this->page->buildElement("notifications", array(
+                "userNotifications" => $notifications
+            ));
+
+        }
+        
+    }
+
+?>
---
+++ modules/installed/notifications/notifications.tpl.php
@@ -0,0 +1,28 @@
+<?php
+
+    class notificationsTemplate extends template {
+        
+        public $notifications = '
+        	{#each userNotifications}
+	        	<div class="well well-sm">
+                    <p class="tiny">
+                    	{#unless read}
+                    		<span class="new">*NEW*</span>
+                    	{/unless}
+                        {date}
+                    </p>
+		        	<p>
+		        		<{text}>
+		        	</p>
+	            </div>
+    		{/each} 
+        	{#unless userNotifications}
+        		<div class="text-center">
+        			<em> You have no notifications</em>
+        		</div>
+	        {/unless}
+        ';
+        
+    }
+
+?>
