fa894843d3349cb4a012743cd71e955c007d89a0
Chris Day <chris@cdcoding.com>
2018-10-08 21:43:18 +0100

    improved UI

--- modules/installed/leaderboards/leaderboards.tpl.php
+++ modules/installed/leaderboards/leaderboards.tpl.php
@@ -6,7 +6,7 @@
 			<a href="?page=leaderboards&top10=money" class="btn btn-default">Richest</a>
 			<a href="?page=leaderboards&top10=rank" class="btn btn-default">Rank</a>
 			<h3>{title}</h3>
-			<table class="table">
+			<table class="table table-striped table-bordered table-condensed">
 				<thead>
 					<tr>
 						<th width="100px">Rank</th>
