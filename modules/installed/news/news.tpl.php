ba410bdb9aeccfc3994ca903b3ba8019b6fc4402
Chris Day <chris@cdcoding.com>
2018-10-08 21:44:00 +0100

    added game news to login page

---
+++ modules/installed/news/news.tpl.php
@@ -0,0 +1,22 @@
+<?php
+
+    class newsTemplate extends template {
+        
+        public $newsArticle = '
+        	{#each news}
+                <div class="well well-sm">
+    	        	<h3>
+                        {title} 
+                        <small class="pull-right tiny">
+                            By {>userName}<br />
+                            {date}
+                        </small>
+                    </h3>
+                    <hr />
+    	        	<p>[{text}]</p>
+                </div>
+        	{/each}
+        ';
+    }
+
+?>
