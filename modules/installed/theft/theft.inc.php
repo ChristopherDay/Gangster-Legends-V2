2f217d0876d173e99e9c96859182db57651bcc20
Christopher Day <chris@cdcoding.com>
2018-11-03 10:28:10 +0000

    you now steal cars depending on value rather then id

--- modules/installed/theft/theft.inc.php
+++ modules/installed/theft/theft.inc.php
@@ -56,7 +56,7 @@
                 $carDamage = mt_rand(1, $theftInfo->T_maxDamage);
                 $userChance = $theftInfo->T_chance + ($this->user->info->US_rank * 2);
                 
-                $cars = $this->db->prepare("SELECT * FROM cars WHERE CA_id <= :maxCar AND CA_id >= :minCar");
+                $cars = $this->db->prepare("SELECT * FROM cars WHERE CA_value <= :maxCar AND CA_value >= :minCar");
                 $cars->bindParam(':minCar', $theftInfo->T_worstCar);
                 $cars->bindParam(':maxCar', $theftInfo->T_bestCar);
                 $cars->execute();
@@ -66,9 +66,7 @@
                 $total = 0;
                 
                 foreach ($cars as $row) {
-                
                     $total += $row['CA_theftChance'];
-                    
                 }
                 
                 $car = mt_rand(1, $total);
@@ -76,46 +74,36 @@
                 $total2 = 0;
                 
                 foreach ($cars as $row) {
-                
                     $total2 += $row['CA_theftChance'];
-                    
                     if ($total2 > $car) {
-                        
                         $car = $row['CA_id'];
                         $carName = $row['CA_name'];
-                        
                         break;
                     }
                     
                 }
+
+				$this->user->updateTimer('theft', $theftTime, true);
                 
                 if ($chance > $userChance && $jailChance == 1) {
-                    
                     $this->html .= $this->page->buildElement('error', array("text"=>'You failed to steal a '.$carName.', you were caught and sent to jail'));
-					
-					$this->user->updateTimer('jail', ($id*35), true);
-                    
+                    $this->user->updateTimer('jail', ($id*35), true);
                 } else if ($chance > $userChance) {
-                    
                     $this->html .= $this->page->buildElement('error', array("text"=>'You failed to steal a '.$carName.'.'));
-                
                 } else {
-                    
                     $this->html .=$this->page->buildElement('success', array("text" => 'You successfuly stole a '.$carName.' with '.$carDamage.'% damage.'));
                     $query = "UPDATE userStats SET US_exp = US_exp + ".$car." WHERE US_id = :uid";
-                	$u = $this->db->prepare($query);
-                	$u->bindParam(':uid', $this->user->info->US_id);
-                	$u->execute();
-					
+                    $u = $this->db->prepare($query);
+                    $u->bindParam(':uid', $this->user->info->US_id);
+                    $u->execute();
+                    
                     $insert = $this->db->prepare("INSERT INTO garage (GA_uid, GA_car, GA_damage, GA_location) VALUES (:uid, :car, :damage, :loc)");
                     $insert->bindParam(':uid', $this->user->info->US_id);
                     $insert->bindParam(':loc', $this->user->info->US_location);
                     $insert->bindParam(':car', $car);
                     $insert->bindParam(':damage', $carDamage);
                     $insert->execute();
-				}
-				
-				$this->user->updateTimer('theft', $theftTime, true);
+                }
             }
         
         }
