40d2898aaf89a80444c60006e61d8198ec743b3b
Christopher Day <chris@cdcoding.com>
2018-11-22 17:31:53 +0000

    finished bullet factory ownership and added restock

--- modules/installed/bullets/bullets.inc.php
+++ modules/installed/bullets/bullets.inc.php
@@ -1,23 +1,85 @@
 <?php
 
     class bullets extends module {
+
+        public $maxCost = 100000;
+        public $bulletCost = 100; 
         
         public $pageName = 'Bullet Factory';
         public $allowedMethods = array('bullets'=>array('type'=>'post'));
+
+        public function setCost($cost) {
+            $this->bulletCost = $cost; 
+            if ($this->bulletCost > $this->maxCost) $this->bulletCost = $this->maxCost; 
+        } 
         
+        public function restock() {
+            $settings = new Settings();
+            $lastRestock = (int) $settings->loadSetting("lastBulletRestock");
+
+            $thisHour = strtotime(date("Y-m-d H:00:00"));
+
+            $hoursSinceLastRestock = floor(($thisHour - $lastRestock) / 3600);
+
+            if ($hoursSinceLastRestock > 12) $hoursSinceLastRestock = 12;
+
+            if (!$hoursSinceLastRestock) return;
+
+            $locations = $this->db->prepare("SELECT * FROM locations");
+            $locations->execute();
+            $allLocations = $locations->fetchAll(PDO::FETCH_ASSOC);
+
+            foreach ($allLocations as $location) {
+                $newBullets = $this->getNewBulletStock($hoursSinceLastRestock);
+                $update = $this->db->prepare("
+                    UPDATE locations SET L_bullets = L_bullets + :qty WHERE L_id = :id
+                ");
+                $update->bindParam(":qty", $newBullets);
+                $update->bindParam(":id", $location["L_id"]);
+                $update->execute();
+            }
+
+            $update = $this->db->query("
+                UPDATE locations SET L_bullets = 40000 WHERE L_bullets > 40000
+            ");
+
+            $lastRestock = (int) $settings->loadSetting("lastBulletRestock");
+
+            $settings->update("lastBulletRestock", $thisHour);
+
+        }
+
+        public function getNewBulletStock($hours) {
+            $minPerHour = 2250;
+            $maxPerHour = 2750;
+
+            $bullets = 0;
+
+            $i = 0;
+            while ($i < $hours) {
+                $bullets += mt_rand($minPerHour, $maxPerHour);
+                $i++;
+            }
+
+            return $bullets;
+
+        }
+
         public function constructModule() {
+
+            $this->restock();
             
             $location = $this->db->prepare("SELECT * FROM locations WHERE L_id = ?");
             $location->execute(array($this->user->info->US_location));
             $loc = $location->fetchObject();
             
-            $this->bulletCost = $loc->L_bulletCost;
+            $this->setCost($loc->L_bulletCost);
             
             $this->property = new Property($this->user, "bullets");
             $owner = $this->property->getOwnership();
 
             if (!!$owner["cost"]) {
-                $this->bulletCost = $owner["cost"];
+                $this->setCost($owner["cost"]);
             }
 
             $owner["locationName"] = $loc->L_name;
@@ -83,13 +145,13 @@
             $location->execute(array($this->user->info->US_location));
             $loc = $location->fetchObject();
 
-            $this->bulletCost = $loc->L_bulletCost;
+            $this->setCost($loc->L_bulletCost);
             
             $this->property = new Property($this->user, "bullets");
             $owner = $this->property->getOwnership();
 
             if (!!$owner["cost"]) {
-                $this->bulletCost = $owner["cost"];
+                $this->setCost($owner["cost"]);
             }
 			
             $cost = ($qty * $this->bulletCost);
@@ -126,7 +188,7 @@
             } else {
             
                 $this->alerts[] = $this->page->buildElement('success', array(
-                    "text"=>'You bought '.$qty.' bullets for $'.number_format($cost)
+                    "text"=>'You brought '.$qty.' bullets for $'.number_format($cost)
                 ));
                 
                 $query = "
@@ -136,13 +198,21 @@
                     WHERE
                         US_id = :id
                 "; 
-                
 				
                 $uUser = $this->db->prepare($query);
                 $uUser->bindParam(":money", $cost);
                 $uUser->bindParam(":id", $this->user->id);
                 $uUser->execute();
 				
+                if ($owner["user"]) {
+                    $profit = $cost * 0.5;
+                    $this->property->updateProfit($profit);
+                    $uUser = $this->db->prepare("UPDATE userStats SET US_bank = US_bank + :money WHERE US_id = :id");
+                    $uUser->bindParam(":id", $owner["user"]["id"]);
+                    $uUser->bindParam(":money", $profit);
+                    $uUser->execute();
+                }
+
 				$this->user->updateTimer('bullets', 60, true);
 				
                 $uLoc = $this->db->prepare("UPDATE locations SET L_bullets = L_bullets - $qty WHERE L_id= :loc");
