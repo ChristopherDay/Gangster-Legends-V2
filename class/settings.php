4a7a60501d4bd8acf6bd13ced9c3721a50465365
Christopher Day <chris@cdcoding.com>
2018-11-30 11:08:44 +0000

    added a simple function to return a parameter, this can be used within the template engine

--- class/settings.php
+++ class/settings.php
@@ -1,5 +1,10 @@
 <?php
 
+	function _setting($setting) {
+		$settings = new settings();
+		return $settings->loadSetting($setting, false);
+	}
+
 	class settings {
 
 		private $page, $user, $db, $debug = false;
@@ -32,40 +37,23 @@
 			$row = $select->fetch(PDO::FETCH_ASSOC);
 			
 			if (empty($row['S_desc'])) {
-				
 				if ($makeIfNotExist) {
-				
 					if (is_array($value)) {
-
 						$value = json_encode($value);
-
 					}
 					$this->makeSetting($setting, $value);
-				
 					return $value;
-					
 				} else {
-					
 					return false;
-					
 				}
-				
 			} else {
-				
 				$value = @json_decode($row['S_value'], true);
-				
 				if (is_array($value)) {
-				
 					return $value;
-					
 				} else {
-				
 					return $row['S_value'];
-				
 				}
-				
 			}
-			
 		}
 		
 		public function makeSetting ($setting, $value) {
@@ -73,11 +61,9 @@
 			if(!$this->loadSetting($setting, false)) {
 				
 				if (is_array($value)) {
-				
 					$value = json_encode($value);
-					
 				}
-			
+
 				$delete = $this->db->prepare("DELETE FROM settings WHERE S_desc = :desc");
 				$delete->bindParam(":desc", $setting);
 				$delete->execute();
@@ -86,7 +72,6 @@
 				$insert->bindParam(":desc", $setting);
 				$insert->bindParam(":value", $value);
 				$insert->execute();
-				
 			}
 			
 		}
@@ -94,9 +79,7 @@
 		public function update($setting, $value) {
 				
 			if (is_array($value)) {
-
 				$value = json_encode($value);
-
 			}
 			
 			$this->loadSetting($setting);
