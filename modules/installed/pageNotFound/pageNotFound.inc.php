f522d726ef962823bbd7c5f13b217ff7da0308aa
Christopher Day <chris@cdcoding.com>
2018-11-30 18:23:52 +0000

    added a page to display when a module is not found

---
+++ modules/installed/pageNotFound/pageNotFound.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+    class pageNotFound extends module {
+        
+        public $allowedMethods = array();
+		
+		public $pageName = '';
+        
+        public function constructModule() {
+            $this->html .= $this->page->buildElement("pageNotFound");
+        }
+        
+    }
+
+?>
---
+++ modules/installed/pageNotFound/pageNotFound.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+    class pageNotFoundTemplate extends template {
+        
+        public $pageNotFound = '
+        	<h2>Something went wrong</h2>
+        	<p> We could not find the page you requested!</p>
+        ';
+        
+    }
+
+?>
