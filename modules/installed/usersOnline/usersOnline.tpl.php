89c382786c263ef82ee2d2411857e76556056402
Christopher Day <chris@cdcoding.com>
2018-12-11 13:27:14 +0000

    updated UI to fit into the current theme

--- modules/installed/usersOnline/usersOnline.tpl.php
+++ modules/installed/usersOnline/usersOnline.tpl.php
@@ -1,20 +1,24 @@
 <?php
 
-    class usersOnlineTemplate extends template {
-    
-        public $usersOnline = '<div class="row">
-        	{#each durations}
-        		<div class="col-md-6">
-        			<h4 class="text-left">{title}</h4>
-        			<ul class="text-left">
-        				{#each users}
-							<li>{>userName}</li>
-        				{/each}
-        			</ul>
-        		</div>
-        	{/each}
-        </div>';
-        
-    }
+	class usersOnlineTemplate extends template {
+	
+		public $usersOnline = '<div class="row">
+			{#each durations}
+				<div class="col-md-6">
+					<h4 class="text-left">{title}</h4>
+					{#each users}
+						<div class="crime-holder">
+							<p>
+								<span class="action">
+								    {>userName} 
+								</span> 
+							</p>
+						</div>
+					{/each}
+				</div>
+			{/each}
+		</div>';
+		
+	}
 
 ?>
