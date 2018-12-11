736dc876c5706ead7c39d5cec1db5ebfdd43a64a
Christopher Day <chris@cdcoding.com>
2018-12-11 13:24:04 +0000

    You can always use the logout module now

--- init.php
+++ init.php
@@ -43,11 +43,13 @@
 			
 			$user->updateTimer('laston', time());
 
-	        if ($user->info->U_status == 0) {
+	        if ($_GET["page"] == "logout") {
+	            $page->loadPage('logout');
+	        } else if ($user->info->U_status == 0) {
 	            $page->loadPage('dead');
 	        } else if ($user->info->U_status == 2) {
 	            $page->loadPage('users');
-	        } else if ($user->info->U_userLevel == 3 && $_GET["page"] != "logout") {
+	        } else if ($user->info->U_userLevel == 3) {
 	            $page->loadPage('banned');
 	        } else if (!$user->checkTimer('jail')) {
 	            if ($jailPageCheck["accessInJail"]) {
