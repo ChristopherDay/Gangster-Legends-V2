d191436fe79e195e165d77b1dab23ea6d0f4ab83
Christopher Day <chris@cdcoding.com>
2019-01-15 13:41:55 +0000

    started work on gangs

---
+++ modules/installed/gang/gang.tpl.php
@@ -0,0 +1,98 @@
+<?php
+
+    class gangTemplate extends template {
+        
+        public $gangOverview = '
+        	<h3>{name}</h3>
+        	{#if desc}
+        		[{desc}]
+        	{/if}
+        	{#unless desc}
+        		<em>
+        			This gang has no description
+        		</em>
+        	{/unless}
+        	<h4>Members</h4>
+        	{#each members}
+	        	<div class="crime-holder"> 
+	        		<p>
+	        			<span class="action">
+	        				{>userName}
+	        			</span>
+	        			<span class="cooldown">
+	        				{gangRank}
+	        			</span>
+	        		</p>
+	        	</div>
+        	{/each}
+        ';
+
+        public $gangs = '
+
+			{#unless locations}
+				<div class="text-center">
+					<em>There are no gangs to join</em>
+				</div>
+			{/unless}        	
+
+	        {#each locations}
+	        	{#if gangID}
+		        	<div class="crime-holder"> 
+		        		<div class="crime-header">
+			        		{locationName}
+			        	</div>
+		        		<p>
+		        			<span class="action">
+		        				{gangName}
+		        			</span>
+		        			<span class="cooldown">
+			        			{>userName}
+			        		</span>
+		        			<span class="commit">
+		        				<a href="?page=gang&action=view&id={gangID}">
+		        					view
+		        				</a>
+		        			</span> 
+		        		</p>
+		                <div class="crime-perc gang-members" data-members="GANG MEMBERS: {members} / {maxMembers}">
+		                    <div class="perc" style="width:{percent}%;"></div>
+		                </div>
+		        	</div>
+		        {/if}
+	        {/each}
+
+	        <div class="clearfix"></div>
+
+	        {#unless user.gang}
+
+		        {#if availableLocations}
+
+			        <h4>Start a new gang</h4>
+			        <p>
+			        	You will need to bribe a government official ${number_format gangCost} to start a gang in their country!
+			        </p>
+			        <form action="?page=gang&action=new" method="post">
+
+			        	<input type="text" name="name" class="form-control form-control-inline" placeholder="Gang Name" />
+
+			        	<select name="location" class="form-control form-control-inline">
+			        		{#each availableLocations}
+			        			<option value="{locationID}">{locationName}</option>
+			        		{/each}
+			        	</select>
+
+			        	<button class="btn">
+			        		Submit
+			        	</button>
+
+			        </form>
+
+		        {/if}
+
+	        {/unless}
+
+        ';
+        
+    }
+
+?>
