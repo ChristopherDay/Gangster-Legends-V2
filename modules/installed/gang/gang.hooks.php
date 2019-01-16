98c44a01754973e382fea8537186bccf8d87f9fc
Christopher Day <chris@cdcoding.com>
2019-01-16 18:40:37 +0000

    Added "My gang" link to gang menu

--- modules/installed/gang/gang.hooks.php
+++ modules/installed/gang/gang.hooks.php
@@ -7,4 +7,12 @@
 			"sort" => 100
 		);
 	});
+
+	new hook("gangMenu", function ($user) {
+		if ($user->info->US_gang) return array(
+			"url" => "?page=gang&action=home", 
+			"text" => "My Gang", 
+			"sort" => 100
+		);
+	});
 ?>
