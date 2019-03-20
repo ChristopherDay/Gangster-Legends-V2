ad2871c13a0696534a8c74147b86ab70abc27299
Christopher Day <chris@cdcoding.com>
2019-03-20 16:58:07 +0000

    Update jail.hooks.php

--- modules/installed/jail/jail.hooks.php
+++ modules/installed/jail/jail.hooks.php
@@ -13,26 +13,29 @@
     new hook("locationMenu", function ($user) {
         global $page;
 
-        $count = $user->db->prepare("
-                SELECT DISTINCT 
-                    `U_id` as 'id'
-                FROM 
-                    `userTimers` `jail`
-                    INNER JOIN `users` ON (U_id = UT_user) 
-                    INNER JOIN `userStats` ON (US_id = UT_user)
-                    LEFT OUTER JOIN `userTimers` as `max` ON (`max`.`UT_desc` = 'superMax' AND `max`.`UT_user` = `jail`.`UT_user`)
-                WHERE 
-                    `US_location` = :location AND 
-                    `jail`.`UT_desc` = 'jail' AND 
-                    `jail`.`UT_time` > UNIX_TIMESTAMP()
-                ORDER BY US_rank ASC, U_name
-        ");
-        $count->bindParam(":location", $user->info->US_location);
-        $count->execute();
+        if ($user) {
+            
+            $count = $user->db->prepare("
+                    SELECT DISTINCT 
+                        `U_id` as 'id'
+                    FROM 
+                        `userTimers` `jail`
+                        INNER JOIN `users` ON (U_id = UT_user) 
+                        INNER JOIN `userStats` ON (US_id = UT_user)
+                        LEFT OUTER JOIN `userTimers` as `max` ON (`max`.`UT_desc` = 'superMax' AND `max`.`UT_user` = `jail`.`UT_user`)
+                    WHERE 
+                        `US_location` = :location AND 
+                        `jail`.`UT_desc` = 'jail' AND 
+                        `jail`.`UT_time` > UNIX_TIMESTAMP()
+                    ORDER BY US_rank ASC, U_name
+            ");
+            $count->bindParam(":location", $user->info->US_location);
+            $count->execute();
 
-        $usersInJail = $count->fetchAll(PDO::FETCH_ASSOC);
+            $usersInJail = $count->fetchAll(PDO::FETCH_ASSOC);
 
-        $page->addToTemplate("jailCount", count($usersInJail));
+            $page->addToTemplate("jailCount", count($usersInJail));
+        }
         return array(
             "url" => "?page=jail", 
             "text" => "Jail"
