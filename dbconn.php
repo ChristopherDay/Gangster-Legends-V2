a6c9be09aa72920b345729840cf63295fe637294
Christopher Day <chris@cdcoding.com>
2019-03-28 22:32:41 +0000

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,5 +1,7 @@
 <?php
 
     $db = NEW PDO("mysql:host=localhost;dbname=GLV2", "root", "");
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, 1);
+     
 ?>
