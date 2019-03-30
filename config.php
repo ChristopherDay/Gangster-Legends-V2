3f83a42eb86859ae983bcd968d119bce36e87192
Christopher Day <chris@cdcoding.com>
2019-03-30 21:40:46 +0000

    moded db connection details into the config file

--- config.php
+++ config.php
@@ -1,8 +1,12 @@
 <?php
 
-    $page->addToTemplate('game_name', 'Game Name'); // The name of your game
-    $page->theme = 'default'; // select the template you want to load
-    $page->adminTheme = 'admin'; // select the template you want to load
-    $config->debug = false;
+    $config = array();
+
+    $config["db"] = array(
+    	"host" => "localhost", 
+    	"database" => "lom",
+    	"user" => "root",
+    	"pass" => ""
+    );
 
 ?>
