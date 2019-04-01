23156c3d4572b5d423e57ec769eab16e243a2ddc
Christopher Day <chris@cdcoding.com>
2019-04-01 15:34:07 +0100

    Update db.php

--- class/db.php
+++ class/db.php
@@ -6,56 +6,39 @@ class glPDO extends PDO {
     parent::__construct($name_host, $username, $password, $driverOptions);
   }
 
-  function update($query, $params = array()) {
+  function runQuery() {
   	$q = parent::prepare($query);
-  	foreach ($params as $key => $value) {
-  		$q->bindParam($key, $value);
-  	}
-  	$q->execute();
+  	$q->execute($params);
   	$q->closeCursor();
   	unset($q);
+  }
+
+  function update($query, $params = array()) {
+  	$this->runQuery($query, $params);
   }  
 
   function delete($query, $params = array()) {
-  	$q = parent::prepare($query);
-  	foreach ($params as $key => $value) {
-  		$q->bindParam($key, $value);
-  	}
-  	$q->execute();
-  	$q->closeCursor();
-  	unset($q);
+  	$this->runQuery($query, $params);
   }  
 
   function insert($query, $params = array()) {
-  	$q = parent::prepare($query);
-  	foreach ($params as $key => $value) {
-  		$q->bindParam($key, $value);
-  	}
-  	$q->execute();
-  	$q->closeCursor();
-  	unset($q);
+  	$this->runQuery($query, $params);
   	return parent::lastInsertId();
   }  
 
   function select($query, $params = array()) {
   	$q = parent::prepare($query);
-  	foreach ($params as $key => $value) {
-  		$q->bindParam($key, $value);
-  	}
-  	$q->execute();
-  	$result = $q->fetchAll(PDO::FETCH_ASSOC);
+  	$q->execute($params);
+  	$result = $q->fetch(PDO::FETCH_ASSOC);
   	$q->closeCursor();
   	unset($q);
-  	if (count($result)) return $result[0];
-  	return array();
+	return $result;
+  	
   }
 
   function selectAll($query, $params = array()) {
   	$q = parent::prepare($query);
-  	foreach ($params as $key => $value) {
-  		$q->bindParam($key, $value);
-  	}
-  	$q->execute();
+  	$q->execute($params);
   	$result = $q->fetchAll(PDO::FETCH_ASSOC);
   	$q->closeCursor();
   	unset($q);
