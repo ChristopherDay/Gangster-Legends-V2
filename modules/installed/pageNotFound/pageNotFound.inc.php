5ca03f799509e4a22cae806acdbf90cf4d5a6354
Christopher Day <cday@pop-os.localdomain>
2019-07-23 15:36:53 +0100

    minor bug fixes

--- modules/installed/pageNotFound/pageNotFound.inc.php
+++ modules/installed/pageNotFound/pageNotFound.inc.php
@@ -7,6 +7,11 @@
         public $pageName = '';
         
         public function constructModule() {
+
+        	if (!isset($this->user->info->U_id)) {
+        		return $this->page->redirectTo("login");
+        	}
+
             $this->html .= $this->page->buildElement("pageNotFound");
         }
         
--- modules/installed/policeChase/policeChase.tpl.php
+++ modules/installed/policeChase/policeChase.tpl.php
@@ -4,17 +4,21 @@
     
         public $policeChase = '
 
+            <div class="alert alert-warning">
+                The police have noticed your driving a stolen car, better get rid of them!
+            </div>
+
             <div class="panel panel-default">
                 <div class="panel-heading">What direction do you want to drive?</div>
                 <div class="panel-body">
                     <div style="text-align:center;">
-                        <a class="button-fixed-width btn" href="?page=policeChase&action=move">Foward</a><br />
-                        <a class="button-fixed-width btn" href="?page=policeChase&action=move">Left</a>
+                        <a class="button-fixed-width btn btn-default" href="?page=policeChase&action=move">Foward</a><br />
+                        <a class="button-fixed-width btn btn-default" href="?page=policeChase&action=move">Left</a>
                         <span class="button-fixed-width move-icon">
                             <i class="glyphicon glyphicon-fullscreen"></i>
                         </span>
-                        <a class="button-fixed-width btn" href="?page=policeChase&action=move">Right</a><br />
-                        <a class="button-fixed-width btn" href="?page=policeChase&action=move">U-Turn</a></p>
+                        <a class="button-fixed-width btn btn-default" href="?page=policeChase&action=move">Right</a><br />
+                        <a class="button-fixed-width btn btn-default" href="?page=policeChase&action=move">U-Turn</a></p>
                     </div>
                 </div>
             </div>
