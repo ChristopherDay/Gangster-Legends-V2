d8e909f8eeec3d9c7714d5fe5ed615bb6491d0a4
Christopher Day <chris@cdcoding.com>
2019-09-20 14:13:11 +0100

    fixed hitting enter on withdraw/deposit

--- modules/installed/bank/bank.tpl.php
+++ modules/installed/bank/bank.tpl.php
@@ -25,42 +25,44 @@
 
         
         public $bank = '
-        <form action="?page=bank&action=process" method="post">
             <div class="row">
                 <div class="col-md-6">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">Deposit</div>
-                        <div class="panel-body">
-                            <p style="height:54px; line-height:18px;">
-                                To launder your money so it is safe to deposit in your bank account will cost you {tax}% of the amount you are going to deposit!
-                            </p>
-                            <p>
-                                <input type="text" class="form-control" value="{deposit}" name="deposit" />
-                            </p>
-                            <p class="text-right">
-                                <button type="submit" class="btn btn-default" name="bank" value="deposit">Deposit</button>
-                            </p>
+                    <form action="?page=bank&action=process" method="post">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">Deposit</div>
+                            <div class="panel-body">
+                                <p style="height:54px; line-height:18px;">
+                                    To launder your money so it is safe to deposit in your bank account will cost you {tax}% of the amount you are going to deposit!
+                                </p>
+                                <p>
+                                    <input type="text" class="form-control" value="{deposit}" name="deposit" />
+                                </p>
+                                <p class="text-right">
+                                    <button type="submit" class="btn btn-default" name="bank" value="deposit">Deposit</button>
+                                </p>
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
                 <div class="col-md-6">
-                    <div class="panel panel-default">
-                        <div class="panel-heading"> Withdraw </div>
-                        <div class="panel-body">
-                            <p style="height:54px; line-height:18px;">
-                                There is no cost to withdraw your money!<br />
-                            </p>
-                            <p>
-                                <input type="text" class="form-control" value="{withdraw}" name="withdraw" />
-                            </p>
-                            <p class="text-right">
-                                <button type="submit" class="btn btn-default" name="bank" value="withdraw">Withdraw</button>
-                            </p>
+                    <form action="?page=bank&action=process" method="post">
+                        <div class="panel panel-default">
+                            <div class="panel-heading"> Withdraw </div>
+                            <div class="panel-body">
+                                <p style="height:54px; line-height:18px;">
+                                    There is no cost to withdraw your money!<br />
+                                </p>
+                                <p>
+                                    <input type="text" class="form-control" value="{withdraw}" name="withdraw" />
+                                </p>
+                                <p class="text-right">
+                                    <button type="submit" class="btn btn-default" name="bank" value="withdraw">Withdraw</button>
+                                </p>
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
-        </form>
 
 
         <form method="post" action="?page=bank&action=transfer">
