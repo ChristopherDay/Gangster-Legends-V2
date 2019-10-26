c58114ef0da8a3663739250000774e64eebd3584
Christopher Day <cday@pop-os.localdomain>
2019-10-26 15:13:16 +0100

    added the unlink

--- install/step/4.php
+++ install/step/4.php
@@ -1,3 +1,31 @@
 <?php
-	heading(4, "Remove Installer");
-	heading(5, "Complete!");
+
+		global $db;
+
+		if ($db) {
+
+			$admins = $db->selectAll("SELECT * FROM users WHERE U_userLevel = 2;");
+			
+
+			if (!count($admins)) {
+
+				if (isset($_GET["remove"])) {
+					success(4, "Remove Installer");
+					echo '<ol><li>Directory removed</li></ol>';
+					unlink("../install");
+					success(5, "Complete!");
+					echo '<ol><li>Redirecting you in 5 seconds</li></ol>';
+					echo '<script>setTimeout(function () { document.location = "../" }, 5000);';
+
+				} else {
+					heading(4, "Remove Installer");
+					echo '<a href="?remove=true" class="btn btn-danger">
+						Finish Install and remove installer
+					</a>';
+					heading(5, "Complete!");
+				}
+
+			} else {
+				heading(4, "Remove Installer");
+				heading(5, "Complete!");
+			}
