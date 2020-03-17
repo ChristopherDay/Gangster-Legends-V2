c8d4ed9449170ce98ab84286dcb7fcc436fbd481
LukeLyons <60277309+LukeLyons@users.noreply.github.com>
2020-03-17 05:17:13 +0000

    Update login.inc.php

--- modules/installed/login/login.inc.php
+++ modules/installed/login/login.inc.php
@@ -15,8 +15,11 @@
             $this->page->addToTemplate("loginPostfix", $settings->loadSetting("loginPostfix"));
 
             $this->html .= $this->page->buildElement('loginForm');
-
             
+            if($this->user->loggedin){
+				header("Location:?page=" . $this->page->landingPage); //check user login so can't access page when loggedin
+                exit;
+            }
         }
         
         public function method_logout() {
