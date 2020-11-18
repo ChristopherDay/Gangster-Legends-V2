d37ae1d2a3246cee94d8abb8704617bdd5dc9e67
Christopher Day <chris@cdcoding.com>
2020-11-18 20:00:01 +0000

    Improved the default layout

--- themes/default/loggedin.php
+++ themes/default/loggedin.php
@@ -50,29 +50,10 @@
         </head>
         <body class="user-status-{userStatus}">
 
-            <div class="mobile-menu">
-                <div class="close-mobile-menu">Close</div>
-                <ul class="nav nav-pills nav-stacked">
-                    {#each menus}
-                        <li class="title col-xs-12"><a href="#">{title}</a></li>
-                        {#each items}
-                            {#if seperator}
-                                <hr />
-                            {/if}
-                            {#unless seperator}
-                                {#unless hide}
-                                    <li class="col-xs-6"><a href="{url}">{text}</a></li>
-                                {/unless}
-                            {/unless}
-                        {/each}
-                    {/each}
-                </ul>
-            </div>
-
             <div class="header">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-4 logo-container text-left">
+                        <div class="col-md-12 logo-container text-center">
                             <button type="button" class="navbar-toggle">
                                 <span class="sr-only">Toggle navigation</span>
                                 <span class="icon-bar"></span>
@@ -81,116 +62,114 @@
                             </button>
                             <img src="themes/{_theme}/images/logo.png" alt="Gangster Legends" />
                         </div>
-                        <div class="col-md-2 col-xs-12 text-center">
-                            <div class="row">
-                                <div class="col-md-12 col-xs-6 text-center">
-                                    <a href="?page=mail">
-                                        Mail{#if mail} ({mail}){/if}<br />
-                                    </a>
-                                </div>
-                                <div class="col-md-12 col-xs-6 text-center">
-                                    <a href="?page=notifications">
-                                        Notifications {#if notificationCount} ({notificationCount}){/if}
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-xs-6 text-center">
-                                <strong class="hidden-xs">Money:</strong> {money} <br />
-                                <div class="hidden-xs">
-                                    <strong>Bullets:</strong> {bullets}
-                                </div>
-                        </div>
-                        <div class="col-md-3 col-xs-6 text-center">
-                            <strong class="hidden-xs">Rank:</strong> 
-                            {rank} {#unless maxRank}({exp_perc}%){/unless}<br />
-                            <div class="hidden-xs">
-                                <strong>Family:</strong> {gang.name}
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
-
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-12 sub-header">
-                        <div class="row">
-                            <div class="col-xs-6 col-sm-2" data-timer-type="name" data-timer="{crime_timer}">
-                                <a href="?page=crimes">
-                                    <span>Crime</span><span></span>
-                                </a>
-                            </div>
-                            <div class="col-xs-6 col-sm-2" data-timer-type="name" data-timer="{theft_timer}">
-                                <a href="?page=theft">
-                                    <span>Theft</span><span></span>
-                                </a>
-                            </div>
-                            <div class="col-xs-6 col-sm-2" data-timer-type="name" data-timer="{chase_timer}">
-                                <a href="?page=policeChase">
-                                    <span>Police Chase</span><span></span>
-                                </a>
-                            </div>
-                            <div class="col-xs-6 col-sm-2" data-timer-type="name" data-timer="{jail_timer}">
-                                <a href="?page=jail">
-                                    <span>Jail</span><span></span>
-                                </a>
-                            </div>
-                            <div class="col-xs-6 col-sm-2" data-timer-type="name" data-timer="{bullet_timer}">
-                                <a href="?page=bullets">
-                                    <span>Bullet Factory</span><span></span>
-                                </a>
-                            </div>
-                            <div class="col-xs-6 col-sm-2" data-timer-type="name" data-timer="{travel_timer}">
-                                <a href="?page=travel">
-                                    <span>Travel</span><span></span>
-                                </a>
-                            </div>
-                        </div>
-                        
-                    </div>
-                </div>
                 
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-3 navigation text-center"> 
+            <div class="container">
+                <div class="side-bar"> 
 
-                            {#each menus}
-                                {#if items}
-                                    <div class="panel panel-default hidden-xs">
-                                        <div class="panel-heading">
-                                            {title}
-                                        </div>
-                                        <div class="panel-body">
-                                            {#each items}
-                                                {#if seperator}
-                                                    <hr />
-                                                {/if}
-                                                {#unless seperator}
-                                                    {#unless hide}
-                                                        {#if url}
-                                                            <a href="{url}">{text}</a> <br />
-                                                        {/if}
-                                                    {/unless}
-                                                {/unless}
-                                            {/each}
-                                        </div>
-                                    </div>
-                                {/if}
-                            {/each}
-
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            User Account
                         </div>
-                        
-                        <div class="col-md-9 game-container text-center">
-                            <div data-ajax-element="alerts" data-ajax-type="html">
-                                <{alerts}>
+                        <div class="list-group">
+                            <div class="list-group-item">
+                                <strong>Username: </strong>
+                                <small class="pull-right">
+                                    {>userName}
+                                </small>
                             </div>
-                            <div data-ajax-element="game" data-ajax-type="html">
-                                <{game}>
+                            <div class="list-group-item">
+                                <strong>Money: </strong>
+                                <small class="pull-right">
+                                    {money}
+                                </small>
+                            </div>
+                            <div class="list-group-item">
+                                <strong>Bullets: </strong>
+                                <small class="pull-right">
+                                    {bullets}
+                                </small>
+                            </div>
+                            <div class="list-group-item">
+                                <strong>{_setting "pointsName"}: </strong>
+                                <small class="pull-right">
+                                    {points}
+                                </small>
+                            </div>
+                            <div class="list-group-item">
+                                <strong>{_setting "gangName"}: </strong>
+                                <small class="pull-right">
+                                    {gang.name}
+                                </small>
+                            </div>
+                            <div class="list-group-item">
+                                <strong>Location: </strong>
+                                <small class="pull-right">
+                                    {location}
+                                </small>
+                            </div>
+                            <div class="list-group-item">
+                                <strong>Health: </strong>
+                                <div class="progress">
+                                    <div class="progress-bar progress-bar-success" style="width: {health}%"></div>
+                                </div>
+                                <small class="pull-right">
+                                    {health}%
+                                </small>
+                            </div>
+                            <div class="list-group-item">
+                                <strong>Rank: </strong>
+                                <div class="progress">
+                                    <div class="progress-bar progress-bar-success" style="width: {exp_perc}%"></div>
+                                </div>
+                                <small class="pull-right">
+                                    {rank}
+                                </small>
                             </div>
                         </div>
                     </div>
+
+
+                    {#each menus}
+                        {#if items}
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    {title}
+                                </div>
+                                <div class="list-group">
+                                    {#each items}
+                                        {#unless hide}
+                                            {#if url}
+                                                <a href="{url}" class="list-group-item">
+                                                    {text}
+                                                    <small class="pull-right">
+                                                        {extra}
+                                                        {#if timer}
+                                                            <span data-timer-type="inline" data-timer="{timer}"></span>
+                                                        {/if}
+                                                    </small>
+                                                </a>
+                                            {/if}
+                                        {/unless}
+                                    {/each}
+                                </div>
+                            </div>
+                        {/if}
+                    {/each}
+
                 </div>
+                    
+                <div class="game-container">
+                    <div data-ajax-element="alerts" data-ajax-type="html">
+                        <{alerts}>
+                    </div>
+                    <div data-ajax-element="game" data-ajax-type="html">
+                        <{game}>
+                    </div>
+                </div>
+
             </div>
 
             <script src="themes/{_theme}/js/jquery.js"></script>
