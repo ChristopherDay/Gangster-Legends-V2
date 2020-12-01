c0609529760ac3f7b2cab4cb0d4fb6fb0fd1639e
Christopher Day <chris@cdcoding.com>
2020-12-01 18:52:01 +0000

    Added an example of the admin table widget

--- modules/installed/stats/stats.hooks.php
+++ modules/installed/stats/stats.hooks.php
@@ -8,4 +8,77 @@
         );
     });
 
-?>
+    new hook("adminWidget-table", function ($user) {
+        
+        global $db;
+
+        $stats = $db->prepare("
+            SELECT 
+                SUM(US_bullets) as 'bullets',
+                SUM(US_points) as 'points',
+                SUM(US_money) + SUM(US_bank) as 'cash', 
+                COUNT(U_id) as 'alive'
+            FROM users INNER JOIN userStats ON (US_id = U_id) 
+            WHERE U_status != 0 AND U_userLevel = 1
+            ORDER BY U_id DESC LIMIT 0, 20
+        ");
+        $stats->execute();
+        $stats = $stats->fetch(PDO::FETCH_ASSOC);
+
+        $deadStats = $db->prepare("
+            SELECT 
+                SUM(US_bullets) as 'bullets',
+                SUM(US_money) + SUM(US_bank) as 'cash', 
+                COUNT(U_id) as 'dead'
+            FROM users INNER JOIN userStats ON (US_id = U_id) 
+            WHERE U_status = 0 
+            ORDER BY U_id DESC LIMIT 0, 20
+        ");
+        $deadStats->execute();
+        $deadStats = $deadStats->fetch(PDO::FETCH_ASSOC);
+
+        return array(
+            "size" => 4, 
+            "title" => "Statistics",
+            "type" => "table", 
+            "header" => array(
+                "columns" => array(
+                    array( "name" => "Stat"),
+                    array( "name" => "#")
+                )
+            ),
+            "data" => array(
+                array(
+                    "columns" => array(
+                        array( "value" => "Alive Users" ),
+                        array( "value" => number_format($stats["alive"]) ),
+                    )
+                ), 
+                array(
+                    "columns" => array(
+                        array( "value" => "Dead Users" ),
+                        array( "value" => number_format($deadStats["dead"]) ),
+                    )
+                ), 
+                array(
+                    "columns" => array(
+                        array( "value" => "Money" ),
+                        array( "value" => "$" . number_format((int) $stats["cash"]) ),
+                    )
+                ), 
+                array(
+                    "columns" => array(
+                        array( "value" => "Bullets" ),
+                        array( "value" => number_format((int) $stats["bullets"]) ),
+                    )
+                ),
+                array(
+                    "columns" => array(
+                        array( "value" => "points" ),
+                        array( "value" => number_format((int) $stats["points"]) ),
+                    )
+                )
+            )
+        );
+
+    });
