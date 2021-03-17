ca81853f6eaddf6c27cbe61e723305b600712b01
Christopher Day <chris@cdcoding.com>
2021-03-17 18:50:44 +0000

    Added core modules to GL

---
+++ modules/installed/search/search.hooks.php
@@ -0,0 +1,10 @@
+<?php
+
+
+    new hook("accountMenu", function () {
+        return array(
+            "url" => "?page=search", 
+            "text" => "Player Search", 
+            "sort" => 900
+        );
+    });
