4eda0b9f5ea75dcf140aa2ab8bfb0f7bb9fe0c59
Christopher Day <chris@cdcoding.com>
2021-04-22 20:02:21 +0100

    added functions to get slots and effects

--- class/items.php
+++ class/items.php
@@ -5,8 +5,9 @@
 		public $settings, $types, $db;
 
 		public function __construct() {
-			global $db;
+			global $db, $page;
 			$this->db = $db;
+			$this->page = $page;
 			$this->settings = new Settings();
 			$types = $this->settings->loadSetting("itemTypes");
 			$this->types = $types;
@@ -14,10 +15,28 @@
 			if (!$this->types) $this->types = array();
 		}
 
+		public function getSlots($user) {
+			$hook = new Hook("equipSlot");
+        	$slots = $this->page->sortArray($hook->run());
+        	$equipSlots = array();
+        	foreach ($slots as $slot) {
+        		$equipSlots[] = array(
+        			"name" => $slot["name"],
+        			"item" => $slot["getItem"]($user), 
+        			"actions" => $slot
+        		);
+        	}
+        	return $equipSlots;
+		}
+
+		public function getEffects() {
+			$hook = new Hook("itemEffects");
+			$effects = $hook->run();
+			return $effects;
+		}
+
 		public function getItem($id) {
-
 			if (!$id) return false; 
-
 			$item = $this->db->select("
 				SELECT 
 					I_id as 'id', 
@@ -32,12 +51,10 @@
 			", array(
 				":id" => $id
 			));
-
 			return $item;
 		}
 
 		public function getType($name, $id = false) {
-				
 			if ($id) {
 				foreach ($this->types as $t) {
 					if ($t["id"] == $id) {
@@ -46,7 +63,6 @@
 				}
 				return false;
 			}
-
 			foreach ($this->types as $t) {
 				if ($t["name"] == $name) {
 					return $t;
@@ -56,9 +72,7 @@
 		}
 
 		public function registerNewType($name, $type) {
-
 			$exists = false;
-
 			$id = 1;
 			foreach ($this->types as $t) {
 				if ($t["name"] == $name) {
@@ -66,16 +80,13 @@
 				}
 				$id++;
 			}
-
 			if (!$exists) {
 				$this->types[] = array(
 					"id" => $id, 
 					"name" => $name, 
 					"type" => $type
 				);
-
 				$this->settings->update("itemTypes", $this->types);
 			}
-
 		}
 	}
