aa4fcfc1086b26c3383a4f5b41f3266672273541
Christopher Day <chris@cdcoding.com>
2021-06-10 10:03:30 +0100

    added sorting

--- modules/installed/admin/admin.admin.php
+++ modules/installed/admin/admin.admin.php
@@ -11,13 +11,14 @@
             if ($tables) {
                 foreach ($tables as $table) {
                     $widgets[] = array(
+                        "sort" => (isset($table["sort"])?$table["sort"]:0),
                         "html" => $this->page->buildElement("widget" . $table["type"], $table)
                     );
                 }
             }
 
             $this->html .= $this->page->buildElement("widgets", array(
-                "widgets" => $widgets
+                "widgets" => $this->page->sortArray($widgets)
             ));
 
         }
