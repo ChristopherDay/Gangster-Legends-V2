3667f8663cfed664346237046b43c6e9322e7d95
Christopher Day <chris@cdcoding.com>
2021-10-30 18:51:45 +0100

    added a simple alert widget for the ACP home page

---
+++ modules/installed/themeManager/themeManager.hooks.php
@@ -0,0 +1,11 @@
+<?php 
+
+    new hook("adminWidget-alerts", function ($user) {
+        global $db, $page;
+        $gameName = _setting("game_name");
+       	if ($gameName == "Game Name") return array( 
+            "type" => "info", 
+            "text" => "The game name is not set, Tto update it please go to the <a href='?page=admin&module=themeManager'>theme manager</a>!"
+        );
+       	return false;
+    });
