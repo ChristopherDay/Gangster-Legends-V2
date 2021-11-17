ac35c6ee657419ffe1023a8dbece8947d755449b
Christopher Day <chris@cdcoding.com>
2021-11-17 19:45:49 +0000

    added a new stats acp widget layout

--- modules/installed/stats/stats.hooks.php
+++ modules/installed/stats/stats.hooks.php
@@ -8,9 +8,42 @@
         );
     });
 
-    new hook("adminWidget-table", function ($user) {
-        
-        global $db, $page;
+    // Shortens a number and attaches K, M, B, etc. accordingly
+    function number_shorten($number, $precision = 3, $divisors = null) {
+
+        // Setup default $divisors if not provided
+        if (!isset($divisors)) {
+            $divisors = array(
+                pow(1000, 0) => '', // 1000^0 == 1
+                pow(1000, 1) => 'K', // Thousand
+                pow(1000, 2) => 'M', // Million
+                pow(1000, 3) => 'B', // Billion
+                pow(1000, 4) => 'T', // Trillion
+                pow(1000, 5) => 'Qa', // Quadrillion
+                pow(1000, 6) => 'Qi', // Quintillion
+            );    
+        }
+
+        // Loop through each $divisor and find the
+        // lowest amount that matches
+        foreach ($divisors as $divisor => $shorthand) {
+            if (abs($number) < ($divisor * 1000)) {
+                // We found a match!
+                break;
+            }
+        }
+
+
+        // We found our match, or there were no matches.
+        // Either way, use the last defined value for $divisor.
+        return number_format($number / $divisor, $precision) . $shorthand;
+    }
+
+    new hook("adminWidget-html", function ($user) {
+
+        global $page, $db;
+
+        $page->registerTemplateFile("modules/installed/stats/widgetStyles.css");
 
         $stats = $db->select("
             SELECT 
@@ -23,58 +56,49 @@
             ORDER BY U_id DESC LIMIT 0, 20
         ");
 
-        $deadStats = $db->select("
-            SELECT 
-                SUM(US_bullets) as 'bullets',
-                SUM(US_money) + SUM(US_bank) as 'cash', 
-                COUNT(U_id) as 'dead'
-            FROM users INNER JOIN userStats ON (US_id = U_id) 
-            WHERE U_status = 0 
-            ORDER BY U_id DESC LIMIT 0, 20
-        ");
+        $html = '
+<!-- thanks https://bootsnipp.com/snippets/rljEW -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+<div class="row">
+    <div class="col-md-3">
+        <div class="card-counter info">
+            <i class="fa fa-users"></i>
+            <span class="count-numbers">'.number_format($stats["alive"]).'</span>
+            <span class="count-name">Users Alive</span>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card-counter primary">
+            <i class="fa fa-money"></i>
+            <span class="count-numbers">'.number_shorten($stats["cash"], 2) .'</span>
+            <span class="count-name">Money</span>
+        </div>
+    </div>
+
+    <div class="col-md-3">
+        <div class="card-counter danger">
+            <i class="fa fa-shield"></i>
+            <span class="count-numbers">'.number_shorten($stats["bullets"], 0) .'</span>
+            <span class="count-name">Bullets</span>
+        </div>
+    </div>
+
+    <div class="col-md-3">
+        <div class="card-counter success">
+            <i class="fa fa-ticket"></i>
+            <span class="count-numbers">'.number_shorten($stats["points"], 0) .'</span>
+            <span class="count-name">Points</span>
+        </div>
+    </div>
+
+</div>
+        ';
 
         return array(
-            "size" => 4, 
-            "title" => "Statistics",
-            "type" => "table", 
-            "header" => array(
-                "columns" => array(
-                    array( "name" => "Stat"),
-                    array( "name" => "#")
-                )
-            ),
-            "data" => array(
-                array(
-                    "columns" => array(
-                        array( "value" => "Alive Users" ),
-                        array( "value" => number_format($stats["alive"]) ),
-                    )
-                ), 
-                array(
-                    "columns" => array(
-                        array( "value" => "Dead Users" ),
-                        array( "value" => number_format($deadStats["dead"]) ),
-                    )
-                ), 
-                array(
-                    "columns" => array(
-                        array( "value" => "Money" ),
-                        array( "value" => $page->money((int) $stats["cash"]) ),
-                    )
-                ), 
-                array(
-                    "columns" => array(
-                        array( "value" => "Bullets" ),
-                        array( "value" => number_format((int) $stats["bullets"]) ),
-                    )
-                ),
-                array(
-                    "columns" => array(
-                        array( "value" => "points" ),
-                        array( "value" => number_format((int) $stats["points"]) ),
-                    )
-                )
-            )
+            "sort" => 0,
+            "size" => 12, 
+            "html" => $html,
+            "type" => "html", 
+            "title" => "Game Statistics"
         );
-
     });
