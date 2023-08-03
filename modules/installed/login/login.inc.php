96c994aa1f180cd26c8f2bab939a8a6d9f113f98
Christopher Day <chris@cdcoding.com>
2023-08-03 19:49:36 +0100

    undid the last change due to support for older themes

--- modules/installed/login/login.inc.php
+++ modules/installed/login/login.inc.php
@@ -29,7 +29,7 @@
         
         public function method_login() {
 
-            if (!$this->checkCSFRToken()) return;
+            //if (!$this->checkCSFRToken()) return;
             
             $round = new Round();
 
